832bd63716ffcff516f0acb505a7ba3159dba2b5
a-yildiz <yildiz@stanford.edu>
2020-12-23 12:08:04 +0300

    Changes to Lookup Mean

--- src/utils/mLookup.jl
+++ src/utils/mLookup.jl
@@ -7,16 +7,42 @@ Lookup mean function
 mutable struct MeanLookup <: GaussianProcesses.Mean
     kdtree::NearestNeighbors.KDTree    # Lookup K-Dimensional Tree of X vals
     Y::AbstractVector{Float64}         # Lookup Y vals
+    num_of_neighbors::Int
+    dist_degree::Int
 
-    MeanLookup(X::AbstractMatrix{Float64}, Y::AbstractVector{Float64}) = new(NearestNeighbors.KDTree(X), Y)
+    MeanLookup(X::AbstractMatrix{Float64}, Y::AbstractVector{Float64}, num_of_neighbors::Int=10, dist_degree::Int=2) = new(NearestNeighbors.KDTree(X), Y, num_of_neighbors, dist_degree)
 end
 
 function GaussianProcesses.mean(mLookup::MeanLookup, x::AbstractVector)
+    nn, dd = knn(mLookup.kdtree, x, mLookup.num_of_neighbors)
+    dd_degreed = inv.(dd.^mLookup.dist_degree)
+    # return mLookup.Y[nn][1]
+    mult = sum(mLookup.Y[nn] .* dd_degreed) / sum(dd_degreed)
+    return mult
+end
+
+function GaussianProcesses.mean(mLookup::MeanLookup, x::AbstractMatrix)
+    num_of_neighbors = mLookup.num_of_neighbors
+    dist_degree = mLookup.dist_degree
+
+    nn, dd = knn(mLookup.kdtree, x, num_of_neighbors)
+    nns = [mLookup.Y[item] for item in nn]
+    dds = [item.^dist_degree for item in dd]
+
+    # global X = x
+    # global MLookup = mLookup
+    mult = Float64[sum(n.*inv.(d))/sum(inv.(d)) for (n,d) in zip(nns, dds)]
+    return mult
+    # dd_degreed = dd.^dist_degree
+    # return mLookup.Y[nn]
+end
+
+function mean_old(mLookup::MeanLookup, x::AbstractVector)
     nn, _ = knn(mLookup.kdtree, x, 1)
     return mLookup.Y[nn][1]
 end
 
-function GaussianProcesses.mean(mLookup::MeanLookup, x::AbstractMatrix)
+function mean_old(mLookup::MeanLookup, x::AbstractMatrix)
     nn, _ = knn(mLookup.kdtree, x, 1)
     nn = [item for sublist in nn for item in sublist]    # extract list of lists
     return mLookup.Y[nn]
